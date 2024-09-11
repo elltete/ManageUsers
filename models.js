7cf1b6d79d3f5f02f059863226f27546aa35ae60
Diego Ponzo <diegoponzo@gmail.com>
2024-09-11 20:07:12 -0300

    Create the project

--- models.js
+++ models.js
@@ -1,7 +1,7 @@
 import { existsSync, readFileSync, writeFileSync} from "node:fs";
 import { randomUUID, createHash } from "node:crypto";
 import "dotenv/config";
-import { handleErrors } from "./utils/handleError.js";
+import { handleError } from "./utils/handleError.js";
 
 const getUsers = () => {
     try {
---
+++ utils/handleError.js
@@ -0,0 +1,10 @@
+import { randomUUID } from "node:crypto";
+import { writeFileSync, readFileSync, existsSync } from "node:fs";
+
+const handleError = (error, path) => {
+
+    
+}
+
+
+export { handleError }
