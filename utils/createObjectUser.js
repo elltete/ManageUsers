90ec327d66dd5eb94f166392268d322cfd4683e6
Diego Ponzo <diegoponzo@gmail.com>
2024-09-11 20:19:41 -0300

    Create the project

---
+++ utils/createObjectUser.js
@@ -0,0 +1,12 @@
+// obtener los args desde index
+
+// desarrollar las funciones
+
+// control de errores
+
+
+const createUserObject = () => {}
+
+const createUpdateUserObject = () => {}
+
+export { createUserObject, createUpdateUserObject }
